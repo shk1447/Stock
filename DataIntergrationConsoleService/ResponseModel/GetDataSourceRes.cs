be28c64d2063388ebd9cc81eb9f36caa58f93be7
KimSeokhwan <shk1447@n3n.co.kr>
2016-09-09 09:11:18 +0900

    commit missing file

---
+++ DataIntergrationConsoleService/ResponseModel/GetDataSourceRes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponseModel
+{
+    public class GetDataSourceRes
+    {
+        private Dictionary<string, object> rawdata;
+
+        public Dictionary<string, object> RawData
+        {
+            get
+            {
+                return this.rawdata;
+            }
+            set
+            {
+                this.rawdata = value;
+            }
+        }
+
+        public GetDataSourceRes()
+        {
+            this.rawdata = new Dictionary<string, object>();
+        }
+    }
+}
