be363a30e23144ff372290a87501ad0a91b9a953
shkim <sukan8822@gmail.com>
2016-09-12 08:08:01 +0900

    change url for naver finance

--- DataIntergrationConsoleService/Modules/NaverFinance/NaverFinance.cs
+++ DataIntergrationConsoleService/Modules/NaverFinance/NaverFinance.cs
@@ -91,7 +91,7 @@ namespace NaverFinance
 
                 WebResponse response;
                 WebRequest request = HttpWebRequest.Create(url.Replace("{pageNumber}","1").Replace("{exchange}",k.ToString()));
-                request.Headers.Add(HttpRequestHeader.Cookie, "NNB=RWW5GGCPUDKFE; npic=53eAXAIznIDf3UUo2oRoj0dLrE7nOVYhmdpFy4/jl4bojhyRPqnOnnT51rEbtKOaCA==; _ga=GA1.2.494051065.1446683975; nx_ssl=2; BMR=s=1469068215609&r=https%3A%2F%2Fm.search.naver.com%2Fsearch.naver%3Fquery%3D%25EB%25B6%2580%25EC%2582%25B0%25ED%2596%2589%26where%3Dm%26sm%3Dmtp_hty&r2=http%3A%2F%2Fm.naver.com%2F; naver_stock_codeList=054940%7C112610%7C000660%7C008700%7C010050%7C051370%7C093230%7C082270%7C099830%7C001740%7C089790%7C002140%7C205100%7C001250%7C000030%7C005720%7C095610%7C123420%7C122870%7C000020%7C; summary_item_type=recent; field_list=12|0000CC12; nid_iplevel=1; page_uid=f52SLdpyLO0ss6ih+G0ssssssRl-199956");
+                request.Headers.Add(HttpRequestHeader.Cookie, "NNB=77DCUJJNP6QFO; npic=QcmfGgCjMM+5v91FvE2Uw4tcWuDRYwcUMC10ZJ8TsvSbQ8QEUYAyC0qRXgypP0zhCA==; nx_ssl=2; field_list=12|0201241");
                 request.Proxy = null;
                 using (response = request.GetResponse())
                 {
@@ -113,7 +113,7 @@ namespace NaverFinance
                 for (int i = 2; i <= lastNumber; i++)
                 {
                     request = HttpWebRequest.Create(url.Replace("{pageNumber}", i.ToString()).Replace("{exchange}", k.ToString()));
-                    request.Headers.Add(HttpRequestHeader.Cookie, "NNB=RWW5GGCPUDKFE; npic=53eAXAIznIDf3UUo2oRoj0dLrE7nOVYhmdpFy4/jl4bojhyRPqnOnnT51rEbtKOaCA==; _ga=GA1.2.494051065.1446683975; nx_ssl=2; BMR=s=1469068215609&r=https%3A%2F%2Fm.search.naver.com%2Fsearch.naver%3Fquery%3D%25EB%25B6%2580%25EC%2582%25B0%25ED%2596%2589%26where%3Dm%26sm%3Dmtp_hty&r2=http%3A%2F%2Fm.naver.com%2F; naver_stock_codeList=054940%7C112610%7C000660%7C008700%7C010050%7C051370%7C093230%7C082270%7C099830%7C001740%7C089790%7C002140%7C205100%7C001250%7C000030%7C005720%7C095610%7C123420%7C122870%7C000020%7C; summary_item_type=recent; field_list=12|0000CC12; nid_iplevel=1; page_uid=f52SLdpyLO0ss6ih+G0ssssssRl-199956");
+                    request.Headers.Add(HttpRequestHeader.Cookie, "NNB=77DCUJJNP6QFO; npic=QcmfGgCjMM+5v91FvE2Uw4tcWuDRYwcUMC10ZJ8TsvSbQ8QEUYAyC0qRXgypP0zhCA==; nx_ssl=2; field_list=12|0201241");
                     request.Proxy = null;
                     using (response = request.GetResponse())
                     {
