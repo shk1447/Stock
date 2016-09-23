945fe1c875eb251e51c947a9ba3d91a7cf3bc7f3
shkim <sukan8822@gmail.com>
2016-09-24 00:28:36 +0900

    add semantic-ui

--- DataIntegrationWebService/client/views/Login.jsx
+++ DataIntegrationWebService/client/views/Login.jsx
@@ -1,6 +1,6 @@
 var React = require('react');
 var Router = require('react-router');
-var {Grid,Row,Col,Button} = require('react-bootstrap');
+var {Form,Grid,Row,Checkbox,Col,Button,FormGroup,ControlLabel,FormControl,HelpBlock} = require('react-bootstrap');
 var io = require('socket.io-client');
 
 module.exports = React.createClass({
@@ -17,16 +17,50 @@ module.exports = React.createClass({
         this.socket.on('login',function(data){
             self.props.history.push('/DataViewer');
         });
-		return {message: 'hoho', values: {}};
+		return { value: '' };
 	},
+    getValidationState() {
+        const length = this.state.value.length;
+        if (length > 10) return 'success';
+        else if (length > 5) return 'warning';
+        else if (length > 0) return 'error';
+    },
+
+    handleChange(e) {
+        this.setState({ value: e.target.value });
+    },
     render : function () {
         return (
-            <Grid>
-                <Row className="show-grid">
-                    <Col sm={4}><Button onClick={this.handleLogin}>LOGIN</Button></Col>
-                    <Col sm={8}>{this.state.message}</Col>
-                </Row>
-            </Grid>
+                <Form horizontal>
+                    <FormGroup controlId="formHorizontalEmail">
+                        <Col componentClass={ControlLabel} lg={2}>
+                            Email
+                        </Col>
+                        <Col sm={4}>
+                            <FormControl type="email" placeholder="Email" />
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup controlId="formHorizontalPassword">
+                        <Col componentClass={ControlLabel} lg={2}>
+                            Password
+                        </Col>
+                        <Col lg={4}>
+                            <FormControl type="password" placeholder="Password" />
+                        </Col>
+                    </FormGroup>
+                    <FormGroup>
+                        <Col lgOffset={5} lg={5}>
+                            <Checkbox>Remember me</Checkbox>
+                        </Col>
+                    </FormGroup>
+
+                    <FormGroup>
+                        <Col lgOffset={5} lg={5}>
+                            <Button type="submit">Sign in</Button>
+                        </Col>
+                    </FormGroup>
+                </Form>
         )
     },
     handleLogin: function (e) {
