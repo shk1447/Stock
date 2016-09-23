85a44f9013a027525b4d6ca3af34e37a5d313f60
KimSeokhwan <shk1447@n3n.co.kr>
2016-09-23 19:57:21 +0900

    commit missing file

---
+++ DataIntegrationConsoleService/WebSocket/Services/DIServiceModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model.Common;
+
+namespace DIWebSocket.Services
+{
+    public class DIServiceRequestModel
+    {
+        public bool broadcast { get; set; }
+
+        public string target { get; set; }
+
+        public JsonDictionary parameters { get; set; }
+    }
+}
