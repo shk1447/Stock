c9eaa9bb7b927e53c91f06faff9634f5a4f1d710
KimSeokhwan <shk1447@n3n.co.kr>
2016-09-28 21:05:09 +0900

    add missing file

---
+++ DataIntegrationWebService/server/router/modules/member.js
@@ -0,0 +1,22 @@
+var jwt = require('jsonwebtoken');
+var secret = null;
+module.exports = {
+	secret: function(key){
+		secret = key;
+	},
+	access: function(_profile) {
+		var profile = {
+			id: _profile.id,
+			password: _profile.password
+		};
+
+		return jwt.sign(profile, secret, {
+			expiresIn: '2 days'
+		});
+	},
+	
+	verify: function(_token, _callback) {
+		// invalid token
+		jwt.verify(_token, secret, _callback);
+	}
+};
