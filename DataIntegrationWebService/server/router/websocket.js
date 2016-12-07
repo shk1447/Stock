791f1896397776160fbc149d9f4738e954016d3f
KimSeokhwan <shk1447@n3n.co.kr>
2016-12-07 17:56:33 +0900

    di service가 올라가있지 않을 경우에 대한 예외처리

--- DataIntegrationWebService/server/router/websocket.js
+++ DataIntegrationWebService/server/router/websocket.js
@@ -7,31 +7,35 @@ module.exports = function (httpServer, config) {
   var io = socket_io.listen(httpServer, { 'destroy buffer size': Infinity });
   io.sockets.on('connection', function(socket){
     socket.on('fromclient', function(data){
-      var d = data; 
-      var ws = new WebSocket(config.url.wsUrl);
-      var sendData = JSON.stringify(data);
-      ws.binaryType = "arraybuffer";
-      ws.on('open', function() {
-        ws.send(sendData);
-      });
-      ws.on('message', function(message) {
-        var msg;
-        var target = d.target.split('.');
-        if(target[1] == "download") {
-          msg = message;
-        } else {
-          msg = JSON.parse(message);
-        }
-        modules[target[0]][target[1]](msg,d);
-        
-        if(d.broadcast) {
-          io.sockets.emit(d.target, msg);
-        } else {
-          socket.emit(d.target, msg);
-        }
-        
-        ws.close();
-      });
+      try {
+        var d = data; 
+        var ws = new WebSocket(config.url.wsUrl);
+        var sendData = JSON.stringify(data);
+        ws.binaryType = "arraybuffer";
+        ws.on('open', function() {
+          ws.send(sendData);
+        });
+        ws.on('message', function(message) {
+          var msg;
+          var target = d.target.split('.');
+          if(target[1] == "download") {
+            msg = message;
+          } else {
+            msg = JSON.parse(message);
+          }
+          modules[target[0]][target[1]](msg,d);
+          
+          if(d.broadcast) {
+            io.sockets.emit(d.target, msg);
+          } else {
+            socket.emit(d.target, msg);
+          }
+          
+          ws.close();
+        }); 
+      } catch (error) {
+        console.log(error);
+      }
     })
   });
 };
