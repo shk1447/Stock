60454a575374f8a4f140476ae4695c2cd6f9ec04
shkim <sukan8822@gmail.com>
2017-01-03 21:10:00 +0900

    aaa

---
+++ DataIntegrationConsoleService/Model/Request/ViewExecuteReq.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Json;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Request
+{
+    public class ViewExecuteReq
+    {
+        public JsonValue name { get; set; }
+
+        public JsonValue member_id { get; set; }
+    }
+}
---
+++ DataIntegrationConsoleService/Model/Response/CommonResponse.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model.Response
+{
+    public class CommonResponse
+    {
+        public string message { get; set; }
+
+        public string code { get; set; }
+    }
+}
